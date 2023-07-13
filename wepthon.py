a39894fcdc4ce31a535285f45f1ca386b55a1502
Wepthon <138310014+Wepthon@users.noreply.github.com>
2023-07-14 01:29:21 +0300

    Update wepthon.py

--- wepthon.py
+++ wepthon.py
@@ -98,7 +98,7 @@ async def _(event):
     end = datetime.datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(f'''
-    await wepthon.send_file(event. chat_id, 'https://t.me/l_raqq/5', caption='''**☆ WELCOME TO WEPTHON
+**☆ WELCOME TO WEPTHON
 ☆ VERSION : 1.3
 ☆ PING : `{ms}`
 ☆ DATE : `{m9zpi}`
